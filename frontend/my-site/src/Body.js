c7034ecac2c34daad41b38651deb59f17e5de424
Anthony <anthony.pizzulli1@gmail.com>
2022-12-15 11:19:55 -0500

    added some body with text

---
+++ frontend/my-site/src/Body.js
@@ -0,0 +1,29 @@
+export default function Body (){
+    return (
+        <div>
+            <header class = "header">
+                Welcome to my site! <b></b> 
+            </header>
+            <p class = "text-body">This is a place for me to showcase my achievements <br></br>
+                from the past few years, in the early stages of my career, as well as some<br></br>
+                personal information/photos! <br></br>
+                
+                <br></br>I designed it all myself using React.js. I wanted to have the freedom<br></br>
+                to use custom styling, and incorporate some of my personal creativity.<br></br> 
+                I have a passion for frontend design, and I thought this would be a great way <br></br>
+                to showcase that.<br></br>
+            </p>
+            <div class="projects-container">
+                <div id="project-col" >
+                    Project 1
+                </div>
+                <div>
+                    Project 2
+                </div>
+                <div>
+                    Project 3
+                </div>
+            </div>
+        </div>
+    );
+}
